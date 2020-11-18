5dac3684110814bef487915e9157882fdbb58e83
Muthu Chandrasekaran <mchandrasekaran@rajant.com>
2020-11-18 12:05:29 -0500

    updated sendRequest in URLSessionClient to upload file instead of data

--- Sources/Apollo/URLSessionClient.swift
+++ Sources/Apollo/URLSessionClient.swift
@@ -124,6 +124,9 @@ open class URLSessionClient: NSObject, URLSessionDelegate, URLSessionTaskDelegat
     
 //    let task = self.session.dataTask(with: request)
 		let data = Data("blah balh".utf8)
+		let tmpDirURL = URL(fileURLWithPath: NSTemporaryDirectory())
+		try? data.write(to: tmpDirURL)
+		
 		let task = self.session.uploadTask(with: request, from: data)
     let taskData = TaskData(rawCompletion: rawTaskCompletionHandler,
                             completionBlock: completion)
